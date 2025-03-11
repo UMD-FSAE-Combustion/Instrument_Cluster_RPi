da9dfb25fa2f6e30416384dee1343035c1822b09
Hussein Chami <hhchami@umich.edu>
2025-03-11 18:55:52 -0400

    Added WiringPi Library

--- gpiohandler.cpp
+++ gpiohandler.cpp
@@ -1,4 +1,13 @@
 #include "gpiohandler.h"
+#include <wiringPi.h>
+#include <iostream>
+
+#define BUTTTON_TOP 23
+#define BUTTTON_BOTTOM 24
+#define BUTTTON_LEFT 27
+#define BUTTTON_RIGHT 22
+
+
 
 GPIOhandler::GPIOhandler(QObject *parent)
     : QObject{parent}
